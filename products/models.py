56b15d1778aead7e3db90c3a6d9a2a1f926a6fd9
Heba Abdulal <hebaabdulal24@gmail.com>
2024-10-24 14:49:57 +0000

    Create a product detail template

--- products/models.py
+++ products/models.py
@@ -8,6 +8,7 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField(default="No description available")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
--- products/views.py
+++ products/views.py
@@ -80,3 +80,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show a specific product in detail """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
