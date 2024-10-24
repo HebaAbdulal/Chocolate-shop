a472db8f93d70918e131dc8ded73796af2aade53
Heba Abdulal <hebaabdulal24@gmail.com>
2024-10-24 11:31:10 +0000

    Add Category and Product models and run migrate

---
+++ products/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    image_url = models.URLField(max_length=500, null=True, blank=True)
+    rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
