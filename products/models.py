f9d647815e41f22e4f9cb20c6115c8cc7ec6a6b0
Heba Abdulal <hebaabdulal24@gmail.com>
2024-10-31 20:55:17 +0000

    Update bag template

--- products/models.py
+++ products/models.py
@@ -23,8 +23,8 @@ class Product(models.Model):
     description = models.TextField(default="No description available")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
-    image_url = models.URLField(max_length=500, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     rating = models.DecimalField(max_digits=2, decimal_places=1, null=True, blank=True)
 
     def __str__(self):
