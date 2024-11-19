74b5aff2bc3a2d2ff0d1c1329f099953edb4ce1f
Heba Abdulal <hebaabdulal24@gmail.com>
2024-11-19 10:00:47 +0000

    Product Admin - Product Form

--- checkout/views.py
+++ checkout/views.py
@@ -8,7 +8,8 @@ import json
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-from profiles.models import UserProfile  # Import UserProfile
+from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 from bag.contexts import bag_contents
 
 @require_POST
@@ -122,9 +123,36 @@ def checkout(request):
     return render(request, template, context)
 
 def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    messages.success(request, f'Order successfully processed! Your order number is {order_number}. A confirmation email will be sent to {order.email}.')
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user to the order
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                    'default_country': order.country,
+                    'default_postcode': order.postcode,
+                    'default_town_or_city': order.town_or_city,
+                    'default_street_address1': order.street_address1,
+                    'default_street_address2': order.street_address2,
+                    'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+    
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
 
     # Remove the bag from the session after successful checkout
     if 'bag' in request.session:
