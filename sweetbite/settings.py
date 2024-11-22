be9d7442bc0d3e6d573a84cf5b5d94d5687e9aed
Heba Abdulal <hebaabdulal24@gmail.com>
2024-11-22 13:56:08 +0000

    Remove sSECRET_KEY and set Debug

--- sweetbite/settings.py
+++ sweetbite/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kpmhop&a1%+ve)06wc#6*kik-_g8c2$17v3%*dscqf)ee1r^j8'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
