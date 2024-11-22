8eeed2ec8516b975fd1b6861f949f004cc3c3a44
Heba Abdulal <hebaabdulal24@gmail.com>
2024-11-22 13:07:24 +0000

    056 deployment part 2

--- sweetbite/settings.py
+++ sweetbite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +31,9 @@ DEBUG = False
 ALLOWED_HOSTS = ['8000-hebaabdulal-chocolatesh-uy2oy5jftah.ws.codeinstitute-ide.net']
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-hebaabdulal-chocolatesh-uy2oy5jftah.ws.codeinstitute-ide.net',
-    '.herokuapp.com'
+    'https://sweetbite.herokuapp.com',
+    'https://127.0.0.1:8000',
+
 ]
 
 
@@ -126,12 +130,17 @@ WSGI_APPLICATION = 'sweetbite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
