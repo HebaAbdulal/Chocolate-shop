c10742cb4487517575f95ae345c94246381e5dee
Heba Abdulal <hebaabdulal24@gmail.com>
2024-12-07 17:43:11 +0000

    Create templates, views and urls of 403 and 500 errors

--- sweetbite/urls.py
+++ sweetbite/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler403, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +30,5 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'sweetbite.views.handler404'
+handler403 = 'sweetbite.views.handler403'
+handler403 = 'sweetbite.views.handler500'
